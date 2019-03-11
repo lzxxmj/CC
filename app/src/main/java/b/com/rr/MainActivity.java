cfc82787688189e17a3df1eece306af328567f9f
mj <majie880103@sina.com>
2019-03-11 10:11:51 +0800

    设置横屏 添加了a变量

--- app/src/main/java/b/com/rr/MainActivity.java
+++ app/src/main/java/b/com/rr/MainActivity.java
@@ -1,13 +1,17 @@
 package b.com.rr;
 
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    int a;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
     }
 }
